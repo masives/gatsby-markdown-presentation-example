32677368dc0973814ef4a59ce820b72b88141f02
Jozef Piecyk <jozef.piecyk@10clouds.com>
2020-01-25 19:53:57 +0100

    simplify full presentation

--- src/templates/presentation.jsx
+++ src/templates/presentation.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+export const pageQuery = graphql`
+  query PresentationBySlug($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+    }
+  }
+`;
+
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark;
@@ -14,24 +22,3 @@ class BlogPostTemplate extends React.Component {
 }
 
 export default BlogPostTemplate;
-
-export const pageQuery = graphql`
-  query PresentationBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-        author
-      }
-    }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        # date(formatString: "MMMM DD, YYYY")
-        description
-      }
-    }
-  }
-`;
